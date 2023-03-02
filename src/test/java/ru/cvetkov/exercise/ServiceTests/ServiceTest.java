fd3bd6f3ecc5262d84fbfea77f5f5436e9ba1be9
YuryTs <tsvetkov_y_g@mail.ru>
2023-03-02 18:09:15 +0300

    43 - tests 02.03

--- src/test/java/ru/cvetkov/exercise/ServiceTests/ServiceTest.java
+++ src/test/java/ru/cvetkov/exercise/ServiceTests/ServiceTest.java
@@ -2,148 +2,145 @@ package ru.cvetkov.exercise.ServiceTests;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.cvetkov.exercise.models.*;
+import ru.cvetkov.exercise.repository.PriceDao;
+import ru.cvetkov.exercise.repository.ProductDao;
 import ru.cvetkov.exercise.service.PriceServiceImpl;
 import ru.cvetkov.exercise.service.ProductServiceImpl;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.List;
+import java.util.*;
 
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 public class ServiceTest {
 
+    @InjectMocks
     private PriceServiceImpl priceServiceImpl;
+    @InjectMocks
     private ProductServiceImpl productServiceImpl;
+    @Mock
+    PriceDao priceDao;
+
+    @Mock
+    ProductDao productDao;
+
 
     @Autowired
-    public ServiceTest(PriceServiceImpl priceServiceImpl, ProductServiceImpl productServiceImpl){
+    public ServiceTest(PriceServiceImpl priceServiceImpl, ProductServiceImpl productServiceImpl) {
         this.priceServiceImpl = priceServiceImpl;
         this.productServiceImpl = productServiceImpl;
     }
 
     @Test
-    void getListByDateTest(){
+    void getListByDateTest() {
+        Product product = new Product(1l, "nokia3310");
+        Price price = new Price();
+        price.setProduct(product);
+        price.setId(1l);
+        price.setDate(LocalDate.parse("2021-09-19"));
+        price.setCost(2900d);
+
+        List<Price> prices = new ArrayList<>();
+        prices.add(price);
+
+        Mockito.doReturn(prices).when(priceDao).getListByDate(LocalDate.now());
         List<PriceDto> priceDtoList = priceServiceImpl.getListByDate(LocalDate.now());
 
         Assertions.assertEquals("nokia3310", priceDtoList.get(0).getName());
         Assertions.assertEquals(2900d, priceDtoList.get(0).getPrice());
-        Assertions.assertEquals("iPhone", priceDtoList.get(1).getName());
-        Assertions.assertEquals(37200d, priceDtoList.get(1).getPrice());
-        Assertions.assertEquals("Mi", priceDtoList.get(2).getName());
-        Assertions.assertEquals(12100d, priceDtoList.get(2).getPrice());
-        Assertions.assertEquals(3, priceDtoList.size());
     }
+
     @Test
     void getCountPriceProductTest() {
+        List<Object[]> list = new ArrayList<>();
+        Object[] objects = {"nokia3310", 2l};
+        list.add(objects);
+
+        Mockito.doReturn(list).when(priceDao).getCountPriceProduct();
         List<StatisticGroupByProduct> statisticGroupByProductList = priceServiceImpl.getCountPriceProduct();
-//        List<Object[]> priceObjects = priceDao.getCountPriceProduct();
-//        List<StatisticGroupByProduct> statisticGroupByProductList = priceObjects.stream()
-//                .map(objects -> {
-//                            String name = (String) objects[0];
-//                            Long frequency = (Long) objects[1];
-//                            return new StatisticGroupByProduct(name, frequency);
-//                        }
-//                ).toList();
 
         Assertions.assertEquals("nokia3310", statisticGroupByProductList.get(0).getName());
-        Assertions.assertEquals(3L, statisticGroupByProductList.get(0).getFrequency());
-        Assertions.assertEquals("iPhone", statisticGroupByProductList.get(1).getName());
-        Assertions.assertEquals(3L, statisticGroupByProductList.get(1).getFrequency());
-        Assertions.assertEquals("Mi", statisticGroupByProductList.get(2).getName());
-        Assertions.assertEquals(2L, statisticGroupByProductList.get(2).getFrequency());
+        Assertions.assertEquals(2L, statisticGroupByProductList.get(0).getFrequency());
     }
 
     @Test
-    void getDateStatisticTest() {
+    void getDateStatisticTest() throws ParseException {
+        List<Object[]> list = new ArrayList<>();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = format.parse("2023-01-09");
+        Object[] objects = {date, 3l};
+        list.add(objects);
+        Mockito.doReturn(list).when(priceDao).getDateStatistic();
         List<StatisticGroupByDate> statisticGroupByDates = priceServiceImpl.getDateStatistic();
-        Assertions.assertEquals("2023-01-09", (statisticGroupByDates.get(0).getDate()).toString());
-        Assertions.assertEquals("2021-09-19", (statisticGroupByDates.get(1).getDate()).toString());
-        Assertions.assertEquals("2023-01-18", (statisticGroupByDates.get(2).getDate()).toString());
-        Assertions.assertEquals("2023-02-01", (statisticGroupByDates.get(3).getDate()).toString());
-        Assertions.assertEquals("2022-09-04", (statisticGroupByDates.get(4).getDate()).toString());
-        Assertions.assertEquals("2020-02-01", (statisticGroupByDates.get(5).getDate()).toString());
-        Assertions.assertEquals("2022-12-01", (statisticGroupByDates.get(6).getDate()).toString());
-        Assertions.assertEquals("2022-10-23", (statisticGroupByDates.get(7).getDate()).toString());
-        Assertions.assertEquals(1,statisticGroupByDates.get(0).getFrequency());
-        Assertions.assertEquals(1,statisticGroupByDates.get(1).getFrequency());
-        Assertions.assertEquals(1,statisticGroupByDates.get(2).getFrequency());
-        Assertions.assertEquals(1,statisticGroupByDates.get(3).getFrequency());
-        Assertions.assertEquals(1,statisticGroupByDates.get(4).getFrequency());
-        Assertions.assertEquals(1,statisticGroupByDates.get(5).getFrequency());
-        Assertions.assertEquals(1,statisticGroupByDates.get(6).getFrequency());
-        Assertions.assertEquals(1,statisticGroupByDates.get(7).getFrequency());
-    }
-    @Test
-    void getGeneralStatisticTest() throws SbException {
-        GeneralProductPriceStatistic generalProductPriceStatistic = priceServiceImpl.getGeneralStatistic();
-        Assertions.assertEquals(3, generalProductPriceStatistic.getCount());
-        Assertions.assertEquals("nokia3310", generalProductPriceStatistic.getStatisticGroupByProductList().get(0).getName());
-        Assertions.assertEquals("iPhone", generalProductPriceStatistic.getStatisticGroupByProductList().get(1).getName());
-        Assertions.assertEquals("Mi", generalProductPriceStatistic.getStatisticGroupByProductList().get(2).getName());
-        Assertions.assertEquals(3L,generalProductPriceStatistic.getStatisticGroupByProductList().get(0).getFrequency());
-        Assertions.assertEquals(3L,generalProductPriceStatistic.getStatisticGroupByProductList().get(1).getFrequency());
-        Assertions.assertEquals(2L,generalProductPriceStatistic.getStatisticGroupByProductList().get(2).getFrequency());
-        Assertions.assertEquals("2023-01-09",(generalProductPriceStatistic.getStatisticGroupByDateList().get(0).getDate()).toString());
-        Assertions.assertEquals("2021-09-19",(generalProductPriceStatistic.getStatisticGroupByDateList().get(1).getDate()).toString());
-        Assertions.assertEquals("2023-01-18",(generalProductPriceStatistic.getStatisticGroupByDateList().get(2).getDate()).toString());
-        Assertions.assertEquals("2023-02-01",(generalProductPriceStatistic.getStatisticGroupByDateList().get(3).getDate()).toString());
-        Assertions.assertEquals("2022-09-04",(generalProductPriceStatistic.getStatisticGroupByDateList().get(4).getDate()).toString());
-        Assertions.assertEquals("2020-02-01",(generalProductPriceStatistic.getStatisticGroupByDateList().get(5).getDate()).toString());
-        Assertions.assertEquals("2022-12-01",(generalProductPriceStatistic.getStatisticGroupByDateList().get(6).getDate()).toString());
-        Assertions.assertEquals("2022-10-23",(generalProductPriceStatistic.getStatisticGroupByDateList().get(7).getDate()).toString());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(0).getFrequency());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(1).getFrequency());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(2).getFrequency());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(3).getFrequency());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(4).getFrequency());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(5).getFrequency());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(6).getFrequency());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(7).getFrequency());
-
+        Date date1 = statisticGroupByDates.get(0).getDate();
+        String dateFromDao = format.format(date1);
+        Assertions.assertEquals("2023-01-09", dateFromDao);
+        Assertions.assertEquals(3, statisticGroupByDates.get(0).getFrequency());
     }
 
     @Test
-    void getStatisticSingleTreadTest(){
-        GeneralProductPriceStatistic generalProductPriceStatistic = priceServiceImpl.getStatisticSingleThread();
-        Assertions.assertEquals(3, generalProductPriceStatistic.getCount());
-        Assertions.assertEquals("nokia3310", generalProductPriceStatistic.getStatisticGroupByProductList().get(0).getName());
-        Assertions.assertEquals("iPhone", generalProductPriceStatistic.getStatisticGroupByProductList().get(1).getName());
-        Assertions.assertEquals("Mi", generalProductPriceStatistic.getStatisticGroupByProductList().get(2).getName());
-        Assertions.assertEquals(3L,generalProductPriceStatistic.getStatisticGroupByProductList().get(0).getFrequency());
-        Assertions.assertEquals(3L,generalProductPriceStatistic.getStatisticGroupByProductList().get(1).getFrequency());
-        Assertions.assertEquals(2L,generalProductPriceStatistic.getStatisticGroupByProductList().get(2).getFrequency());
-        Assertions.assertEquals("2023-01-09",(generalProductPriceStatistic.getStatisticGroupByDateList().get(0).getDate()).toString());
-        Assertions.assertEquals("2021-09-19",(generalProductPriceStatistic.getStatisticGroupByDateList().get(1).getDate()).toString());
-        Assertions.assertEquals("2023-01-18",(generalProductPriceStatistic.getStatisticGroupByDateList().get(2).getDate()).toString());
-        Assertions.assertEquals("2023-02-01",(generalProductPriceStatistic.getStatisticGroupByDateList().get(3).getDate()).toString());
-        Assertions.assertEquals("2022-09-04",(generalProductPriceStatistic.getStatisticGroupByDateList().get(4).getDate()).toString());
-        Assertions.assertEquals("2020-02-01",(generalProductPriceStatistic.getStatisticGroupByDateList().get(5).getDate()).toString());
-        Assertions.assertEquals("2022-12-01",(generalProductPriceStatistic.getStatisticGroupByDateList().get(6).getDate()).toString());
-        Assertions.assertEquals("2022-10-23",(generalProductPriceStatistic.getStatisticGroupByDateList().get(7).getDate()).toString());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(0).getFrequency());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(1).getFrequency());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(2).getFrequency());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(3).getFrequency());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(4).getFrequency());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(5).getFrequency());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(6).getFrequency());
-        Assertions.assertEquals(1,generalProductPriceStatistic.getStatisticGroupByDateList().get(7).getFrequency());
+    void getGeneralStatisticTest() throws ParseException, SbException {
+        Long count = 5l;
+        Mockito.doReturn(count).when(productDao).count();
+//        Mockito.doReturn(productDao.count()).when(productServiceImpl).getCount();
 
+        List<Object[]> list = new ArrayList<>();
+        Object[] objects = {"nokia3310", 2l};
+        list.add(objects);
+        Mockito.doReturn(list).when(priceDao).getCountPriceProduct();
+        List<StatisticGroupByProduct> groupByProducts = priceServiceImpl.getCountPriceProduct();
+
+
+        List<Object[]> list1 = new ArrayList<>();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = format.parse("2023-01-09");
+        Object[] objects1 = {date, 3l};
+        list1.add(objects1);
+        Mockito.doReturn(list1).when(priceDao).getDateStatistic();
+        List<StatisticGroupByDate> groupByDateList = priceServiceImpl.getDateStatistic();
+
+        GeneralProductPriceStatistic generalList = new GeneralProductPriceStatistic(count, groupByProducts, groupByDateList);
+
+        Assertions.assertEquals(generalList.toString(), (priceServiceImpl.getGeneralStatistic()).toString());
     }
 
     @Test
-    void getProductByIdTest(){
-        PriceDto priceDto = productServiceImpl.getById(3);
-        Assertions.assertEquals("Mi",priceDto.getName());
-        Assertions.assertEquals(12100d, priceDto.getPrice());
+    void getProductByIdTest() {
+        Product product = new Product(1l, "nokia3310");
+        Price price = new Price();
+        price.setProduct(product);
+        price.setId(1l);
+        price.setDate(LocalDate.parse("2021-09-19"));
+        price.setCost(2900d);
+        List<Price> prices = new ArrayList<>();
+        prices.add(price);
+        product.setPrices(prices);
+        PriceDto dtoExcpected = new PriceDto(product.getName(), price.getCost());
+        Optional<Product> productOptional = Optional.of(product);
+
+        when(productDao.findById(1l)).thenReturn(productOptional);
+        PriceDto priceDto = productServiceImpl.getById(1);
+
+        Assertions.assertEquals(dtoExcpected, priceDto);
     }
 
     @Test
-    void grtCountTest(){
+    void getCountTest() {
+        Mockito.doReturn(3l).when(productDao).count();
         Long count = productServiceImpl.getCount();
-        Assertions.assertEquals(3, count);
+        Assertions.assertEquals(3l, count);
     }
 
 }
